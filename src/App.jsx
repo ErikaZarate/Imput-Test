143e5571edd42e9e96a4a127b4c02d3d449a0a61
Erika Zarate Gutierrez <erikazg20@gmail.com>
2023-04-17 09:42:39 -0600

    correccion

--- src/App.jsx
+++ src/App.jsx
@@ -122,48 +122,7 @@ function App() {
       </label>
       <br/>
       <label>
-        Fileimport { useState } from 'react'
-
-function App() {
-  const [inputValue,setInputValue] = useState({
-    input1: "",
-    input2: "",
-  });
-  const handleInputChange = (e) => {
-    setInputValue({
-      ...inputValue,
-      [e.target.name]: e.target.value,
-    });
-  }
-
-  return (
-    <div style={{textAlign: "center"}}>
-      <h1>Input Test</h1>
-      <hr />
-      <label htmlFor='in1'>Input 1</label>
-      <input
-        id='in1'
-        name='input1'
-        onChange={(e)=>handleInputChange(e)}
-        type="text"
-        value={inputValue.input1}
-      />
-      <br/>
-      <label>
-        Input 2
-        <input
-          className='mt-2'
-          name='input2'
-          onChange={(e)=>handleInputChange(e)}
-          type="text"
-          value={inputValue.input2}
-        />
-      </label>
-    </div>
-  )
-}
-
-export default App
+        File
         <input
           className='mt-2'
           name='file'
